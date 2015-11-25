8fa66f5add4475c18ae265b1692c0f1f10faf432
BorisovaK <katrin.borisova15@gmail.com>
2015-11-25 16:59:03 +0200

    Homework 2

---
+++ ConditionHW/CardGame/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What card did you chose?");
+            string card = (Console.ReadLine());
+            switch (card)
+            {
+                case "2":
+                    Console.WriteLine("The card is valid");
+                    break;
+                case "3":
+                    Console.WriteLine("The card is valid");
+                    break;
+                case "4":
+                    Console.WriteLine("The card is valid");
+                    break;
+                case "5":
+                    Console.WriteLine("The card is valid");
+                    break;
+                case "6":
+                    Console.WriteLine("The card is valid");
+                    break;
+                case "7":
+                    Console.WriteLine("The card is valid");
+                    break;
+                case "8":
+                    Console.WriteLine("The card is valid");
+                    break;
+                case "9":
+                    Console.WriteLine("The card is valid");
+                    break;
+                case "10":
+                    Console.WriteLine("The card is valid");
+                    break;
+                case "J":
+                    Console.WriteLine("The card is valid");
+                    break;
+                case "Q":
+                    Console.WriteLine("The card is valid");
+                    break;
+                case "K":
+                    Console.WriteLine("The card is valid");
+                    break;
+                case "A":
+                    Console.WriteLine("The card is valid");
+                    break;
+                default:
+                    Console.WriteLine("The card is not valid");
+                    break;
+            }
+        }
+    }
+}
+
---
+++ ConditionHW/PickUpOneType/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickUpOneType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick up one type - string, double or int:");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "int":
+                    int n = int.Parse(Console.ReadLine());
+                    Console.WriteLine(++n);
+                    break;
+
+                case "double":
+                    double f = double.Parse(Console.ReadLine());
+                    Console.WriteLine(++f);
+                    break;
+
+                case "string":
+                    string s = Console.ReadLine();
+                    Console.WriteLine(s + "*");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice!");
+                    break;
+            }
+        }
+    }
+}
+
